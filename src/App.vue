c18c1357867018b27c4c51afdb635d1e03c72a18
mary-metilda <mary.metilda@vonnue.com>
2024-03-01 18:24:12 +0530

    feat: initiate todo app project in vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+</script>
+
+<template>
+  <TodoHeader />
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style> 
+
