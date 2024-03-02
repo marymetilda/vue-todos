66e005b7d902c8539205bdfdcfcda2d3ee89f803
mary-metilda <mary.metilda@vonnue.com>
2024-03-02 15:23:04 +0530

    feat: complete todo app

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <button>
+        <slot>
+            create
+        </slot>
+    </button>
+</template>
+
+
+<style lang="scss" scoped>
+button {
+    padding: 8px 16px;
+    border: none;
+}
+</style>
