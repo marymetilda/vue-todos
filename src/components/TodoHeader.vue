377b5ba2b1fe2a16370df6ff906e38e3b7df7e4d
mary-metilda <mary.metilda@vonnue.com>
2024-06-07 17:42:48 +0530

    feat: update theme

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,60 +1,56 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 </script>
 
-
 <template>
-    <header>
-        <nav class="container">
-            <div class="branding">
-                <img src="../assets/vue-logo-black.png" alt="">
-                <h1>Vue Todos</h1>
-            </div>
-            <ul class="nav-routes">
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </ul>
-        </nav>
-    </header>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <h1 class="text">My Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
 </template>
-
-
 
 <style lang="scss" scoped>
 header {
-    background-color: #f1f1f1;
+  background-color: #f1f1f1;
 
-    nav {
-        display: flex;
-        align-items: center;
-        padding: 25px 16px;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
 
-        .branding {
-            display: flex;
-            align-items: center;
-            gap: 8px;
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
 
-            img {
-                max-width: 50px;
-            }
+      img {
+        max-width: 50px;
+      }
 
-            h1 {
-                font-size: 24px;
-            }
-        }
-
-        .nav-routes {
-            display: flex;
-            flex: 1;
-            justify-content: flex-end;
-            gap: 12px;
-            list-style: none;
-
-            a {
-                text-decoration: none;
-                color: inherit;
-            }
-        }
+      h1 {
+        font-size: 24px;
+      }
     }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text">Here We Go!</h1>
+    <p class="text">
+      Let's mark down our important tasks and mark it according to it's updates!
+    </p>
+    <p class="text">Small Steps leads to a Great Success!</p>
   </div>
 </template>
 
 <style>
+h1,
+p {
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+}
+
+h1 {
+  padding-bottom: 8px;
+}
+
+p {
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+.text {
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: rgb(0 0 0 / 20%);
+}
+
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    height: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    padding-top: 40px;
   }
 }
 </style>
